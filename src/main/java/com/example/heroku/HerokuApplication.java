0f685cdec1b87554d9b84a165ccbf1eea1939ebe
Berto Cisneros <bertocisneros@u.boisestate.edu>
2021-11-27 22:24:24 -0700

    Modified db method

--- src/main/java/com/example/heroku/HerokuApplication.java
+++ src/main/java/com/example/heroku/HerokuApplication.java
@@ -27,12 +27,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.sql.DataSource;
+
+import java.nio.charset.Charset;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.Random;
 
 @Controller
 @SpringBootApplication
@@ -58,7 +61,7 @@ public class HerokuApplication {
     try (Connection connection = dataSource.getConnection()) {
       Statement stmt = connection.createStatement();
       stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+      stmt.executeUpdate("INSERT INTO ticksname VALUES (now(), '" + getRandomString() + "')");
       ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
 
       ArrayList<String> output = new ArrayList<String>();
@@ -72,6 +75,13 @@ public class HerokuApplication {
       model.put("message", e.getMessage());
       return "error";
     }
+  }
+  public String getRandomString(){
+    String str = "";
+    byte[] array = new byte[7]; // length is bounded by 7
+    new Random().nextBytes(array);
+    str = new String(array, Charset.forName("UTF-8"));
+    return str;
   }
 
   @Bean

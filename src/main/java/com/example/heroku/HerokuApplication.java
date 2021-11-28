896384d288a761361863198d72f48f926145179b
Berto Cisneros <bertocisneros@u.boisestate.edu>
2021-11-27 23:07:41 -0700

    Adding print statement

--- src/main/java/com/example/heroku/HerokuApplication.java
+++ src/main/java/com/example/heroku/HerokuApplication.java
@@ -71,7 +71,7 @@ public class HerokuApplication {
       stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
       stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
       ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
+      System.out.println("Print statement from inside the db method. Berto Cisneros");
       ArrayList<String> output = new ArrayList<String>();
       while (rs.next()) {
         output.add("Read from DB: " + rs.getTimestamp("tick") + " " + getRandomString());
